562477ddcd3dc57f5023b79520d8b933b095ae9f
Aditya <adityasorathiya.4112@gmail.com>
2026-04-22 11:00:56 +0530

    Add user management functions for Firebase

--- HelperFunctions/firebase.py
+++ HelperFunctions/firebase.py
@@ -11,3 +11,12 @@ def get_data(slug):
     url = f"{FIREBASE_URL}/{slug}.json"
     res = requests.get(url)
     return res.json()
+
+
+def get_user(email):
+    url = f"{FIREBASE_URL}/users/{email.replace('.', '_')}.json"
+    return requests.get(url).json()
+
+def save_user(email, data):
+    url = f"{FIREBASE_URL}/users/{email.replace('.', '_')}.json"
+    requests.put(url, json=data)
